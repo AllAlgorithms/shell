e587bcb533a8c52f87a09c75801d928578b65cc0
Mathieu Vidalies <5948608+Furinkazan33@users.noreply.github.com>
2018-10-26 16:20:15 +0200

    Fix #3, better coding
    Reccursive function

--- math/factorial.sh
+++ math/factorial.sh
@@ -1,25 +1,14 @@
 #!/usr/bin/env bash
 # run :- bash factorial.sh <your number>
 
-#first argument
-counter=$1
-
-# exit if no args provided
-if [ $# -eq 0 ];then
-	echo "Usage: bash factorial.sh <your number>"
-	exit 1
-fi
+# Usage
+[ $# -ne 1 ] && { echo "Usage: bash factorial.sh <your number>"; exit 1; }
 
 factorial() {
-	fact=1
-	temp=$counter
-	while [ $temp -gt 0 ] #while counter > 0
-	do
-		fact=$(( $fact * $temp ))
-		temp=$(( $temp - 1 ))
-	done
-	echo "$fact"
+    [ $1 -eq 0 ] && { echo 1; return 0; }
+    
+    echo $(($1 * $(factorial $(($1 - 1)))))
 }
 
-# call function
-echo "factorial of $counter = $(factorial)"
+# Test
+echo "factorial of $1 = $(factorial $1)"

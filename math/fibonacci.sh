da6d79782f844b3f7c02f885e226b4148e38aaa3
Mathieu Vidalies <5948608+Furinkazan33@users.noreply.github.com>
2018-10-26 16:24:18 +0200

    Fix #3, better coding

--- math/fibonacci.sh
+++ math/fibonacci.sh
@@ -1,14 +1,8 @@
 #!/usr/bin/env bash
 # run :- bash fibonacci.sh <your number>
 
-# input fibonacci limit
-limit=$1
-
-# exit if no args provided
-if [ $# -eq 0 ];then
-	echo "Usage: bash fibonacci.sh <your number>"
-	exit 1
-fi
+# Usage
+[ $# -ne 1 ] && { echo "Usage: bash fibonacci.sh <your number>"; exit 1; }
 
 fibonacci() {
 	prev=0
@@ -16,7 +10,7 @@ fibonacci() {
 	fib[0]=$prev
 	fib[1]=$current
 	i=2
-	while [ $i -lt $limit ] ; do
+	while [ $i -lt $1 ] ; do
 		sum=$(( $prev + $current ))
 		fib[i]=$sum
 		((++i))
@@ -27,4 +21,4 @@ fibonacci() {
 }
 
 # call function
-echo "fibonacci series with $limit terms = $(fibonacci)"
+echo "fibonacci series with $1 terms = $(fibonacci $1)"

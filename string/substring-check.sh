0be67902f52059ba71be3911e3977e2df0047fa5
Mathieu Vidalies <5948608+Furinkazan33@users.noreply.github.com>
2018-10-26 16:46:49 +0200

    Fix #3
    Some improvement.
    Can read a file
    Better coding

--- string/substring-check.sh
+++ string/substring-check.sh
@@ -1,4 +1,14 @@
-string='My long string'
-if [[ $string = *"My long"* ]]; then
-  echo "It's there!"
-fi
+text='My long string'
+string='y long '
+
+
+find() {
+    while read line; do
+        [[ "$line" = *"$@"* ]] && { echo "Found !"; return 0; }
+    done 
+    
+    echo "Not found !"; return 1;
+} < "${2-/dev/stdin}"
+
+
+echo $text | find "$string"
